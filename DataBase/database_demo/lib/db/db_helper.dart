e4d16f2a8b9aea87797230b4949d0192e706baaa
Jeet Karena <jeetkarena3@gmail.com>
2023-03-13 21:21:43 +0530

    some config on database helper and pages added

--- DataBase/database_demo/lib/db/db_helper.dart
+++ DataBase/database_demo/lib/db/db_helper.dart
@@ -1,9 +1,19 @@
+import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+
+/* 
+    For Access Database in same file we Use
+    var db = instance.dataBase;
+    
+    For Access Database in outer files we Use
+    var db = DatabaseHelper.instance;
+
+ */
 
 class DataBaseHelper {
   // database Veraible For getter
@@ -17,6 +27,8 @@ class DataBaseHelper {
     if (_dataBase != null) {
       return _dataBase;
     }
+    // Call Only First Time When App runes
+    // and then comment down it => copyPasteAssetFileToRoot();
     await copyPasteAssetFileToRoot();
     _dataBase = await _initDatabase();
     return _dataBase;
@@ -26,13 +38,15 @@ class DataBaseHelper {
     Directory appDocDir = await getApplicationDocumentsDirectory();
     // Replcae Your Database Name Here
     String databasePath = join(appDocDir.path, 'temp.db');
-    return await openDatabase(databasePath);
+    log("Database is init.");
+    return await openDatabase(databasePath, version: 1);
   }
 
   Future<bool> copyPasteAssetFileToRoot() async {
     Directory documentsDirectory = await getApplicationDocumentsDirectory();
+    log("Assets Db is Copying");
     // Replcae Your Database Name Here
-    String path = join(documentsDirectory.path, "Demo.db");
+    String path = join(documentsDirectory.path, "temp.db");
 
     if (FileSystemEntity.typeSync(path) == FileSystemEntityType.notFound) {
       // Replcae Your Database File Name Insted Of temp.db
@@ -43,5 +57,11 @@ class DataBaseHelper {
       return true;
     }
     return false;
+  }
+
+  Future<List<Map<String, dynamic>>> getAllStudents() async {
+    Database db = await instance.dataBase;
+    var res = await db.rawQuery("select * from student");
+    return res;
   }
 }
---
+++ DataBase/database_demo/lib/pages/homepage.dart
@@ -0,0 +1,45 @@
+import 'package:database_demo/db/db_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Database"),
+      ),
+      body: FutureBuilder(
+        builder: (context, snapshot) {
+          if (!snapshot.hasData || snapshot.hasError) {
+            return const Center(child: CircularProgressIndicator.adaptive());
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: const CircleAvatar(),
+                  title: Text(
+                    snapshot.data![index]["name"],
+                  ),
+                  subtitle: Text(
+                    snapshot.data![index]["email"],
+                  ),
+                );
+              },
+            );
+          }
+        },
+        future: DataBaseHelper.instance.getAllStudents(),
+      ),
+    );
+  }
+}
